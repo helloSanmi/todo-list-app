f82d4b87c493150b6dc89da6d492484cb19bc639
Oluwasanmi Idowu <idowuoluwasanmi@gmail.com>
2022-06-06 22:33:31 +0100

    update

--- src/TodoDisplay.js
+++ src/TodoDisplay.js
@@ -1,7 +1,10 @@
 import React from "react";
 import "./App.css";
-import {RiCloseCircleLine} from 'react-icons/ri'
-import {TiEdit} from 'react-icons/ti'
+import {RiCloseCircleLine} from 'react-icons/ri';
+import {TiEdit} from 'react-icons/ti';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee, faPen, faTrash } from '@fortawesome/free-solid-svg-icons'
+
 
 const TodoDisplay = ({ arr, setArr, updateValue }) => {
 
@@ -18,14 +21,19 @@ const TodoDisplay = ({ arr, setArr, updateValue }) => {
                 <ul className="display">
                   <li key={index.toString()} className="styleList">{index + 1}. {val}</li>
                   <div>
-                      <RiCloseCircleLine  onClick={deleteTodo(index)}  className="delete-icon btn-del"/>
-                      <TiEdit className="update-icon btn-del" onClick={updateValue(index, val)} />
+
+                      <FontAwesomeIcon icon={faPen} className="update-icon btn-del fa-beat" onClick={updateValue(index, val)}/>
+                      <FontAwesomeIcon icon={faTrash} onClick={deleteTodo(index)}  className="delete-icon btn-del fa-fade" />
+
+                      {/* <RiCloseCircleLine  onClick={deleteTodo(index)}  className="delete-icon btn-del"/> */}
+                      {/* <TiEdit className="update-icon btn-del" onClick={updateValue(index, val)} /> */}
+                      
                       {/* <button className="btn-del" type="button" onClick={deleteTodo(index)}>Delete</button> */}
                       {/* <button className="btn-del" onClick={updateValue(index, val)}>Update</button> */}
                   </div>
                 </ul>
               </>
-          )
+          );
       })}
     </div>
   );
